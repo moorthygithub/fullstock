203759da79d2e5aef2e30f50cc1132a993a27c89
unknown <moorthy.chandiran21@gmail.com>
2025-05-29 18:19:22 +0530

    changes done in the all conditions based thisis new stucture

--- src/json/logo.jsx
+++ src/json/logo.jsx
@@ -1,7 +1,24 @@
+// import React from "react";
+// import image from "../assets/logo.png";
+// import image1 from "../assets/el.png";
+
+// const Logo = ({ showLogo }) => {
+//   return (
+//     <>
+//       {showLogo === "true" ? (
+//         <img src={image1} alt="logo" className="h-8 w-full" />
+//       ) : (
+//         <img src={image} alt="logo" className="w-[200px] mx-auto" />
+//       )}
+//     </>
+//   );
+// };
+
+// export default Logo;
 import React from "react";
 import image from "../assets/img/companylogo.png";
 const Logo = () => {
-  return <img src={image} alt="Logo" className="w-7 h-7" />;
+  return <img src={image} alt="Logo" className="w-8 h-8" />;
 };
 
 export default Logo;
