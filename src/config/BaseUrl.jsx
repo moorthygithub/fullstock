c7e31ebafefd8e2785c3335b5c17a158ec87d9e2
AGS3 <moorthy.chandiran21@gmail.com>
2025-08-13 19:06:59 +0530

    url updates

--- src/config/BaseUrl.jsx
+++ src/config/BaseUrl.jsx
@@ -1,7 +1,5 @@
-const BASE_URL = "https://agsdemo.in/fullstockapi/public/api";
+const BASE_URL = "https://bizstock.in/public/api";
 export default BASE_URL;
 
-export const IMAGE_URL =
-  "https://fullstock.agsdemo.in/images/item_images/";
-export const NO_IMAGE_URL =
-  "https://fullstock.agsdemo.in/images/no_image.jpg";
+export const IMAGE_URL = "https://admin.bizstock.in/images/item_images/";
+export const NO_IMAGE_URL = "https://admin.bizstock.in/images/no_image.jpg";
